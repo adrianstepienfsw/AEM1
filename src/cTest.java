c406feb6758c30d8dacf9479c1e167d5800262cf
Wojciech Młyńczak <wojciech.mlynczak@cognifide.com>
2020-04-26 15:10:43 +0200

    Comment base algorithms for exercise 3

--- src/cTest.java
+++ src/cTest.java
@@ -24,8 +24,8 @@ public class cTest {
         //cAlgorithmLocalSearchGreedyCornersChanging localSearchGreedyCornersSample2 = new cAlgorithmLocalSearchGreedyCornersChanging(sample2, 0.5f);
         //cAlgorithmLocalSearchSteepestPointsChanging localSearchSteepestPointsSample1 = new cAlgorithmLocalSearchSteepestPointsChanging(sample1, 0.5f);
         //cAlgorithmLocalSearchSteepestPointsChanging localSearchSteepestPointsSample2 = new cAlgorithmLocalSearchSteepestPointsChanging(sample2, 0.5f);
-        cAlgorithmLocalSearchSteepestCornersChanging localSearchSteepestCornersSample1 = new cAlgorithmLocalSearchSteepestCornersChanging(sample1, 0.5f);
-        cAlgorithmLocalSearchSteepestCornersChanging localSearchSteepestCornersSample2 = new cAlgorithmLocalSearchSteepestCornersChanging(sample2, 0.5f);
+        //cAlgorithmLocalSearchSteepestCornersChanging localSearchSteepestCornersSample1 = new cAlgorithmLocalSearchSteepestCornersChanging(sample1, 0.5f);
+        //cAlgorithmLocalSearchSteepestCornersChanging localSearchSteepestCornersSample2 = new cAlgorithmLocalSearchSteepestCornersChanging(sample2, 0.5f);
 
         cAlgorithmLocalSearchSteepestCornersChangingListMoves localSearchSteepestCornersListMovesSample1 = new cAlgorithmLocalSearchSteepestCornersChangingListMoves(sample1, 0.5f);
         cAlgorithmLocalSearchSteepestCornersChangingListMoves localSearchSteepestCornersListMovesSample2 = new cAlgorithmLocalSearchSteepestCornersChangingListMoves(sample2, 0.5f);
@@ -43,35 +43,32 @@ public class cTest {
         //localSearchGreedyCornersSample2.make();
         //localSearchSteepestPointsSample1.make();
         //localSearchSteepestPointsSample2.make();
-        localSearchSteepestCornersSample1.testAlgorithm(100, "Making Local Search Steepest (changing with corners) Algorithm for :"+sample1.name);
-        localSearchSteepestCornersSample2.testAlgorithm(100, "Making Local Search Steepest (changing with corners) Algorithm for :"+sample1.name);
+        //localSearchSteepestCornersSample1.testAlgorithm(100, "Making Local Search Steepest (changing with corners) Algorithm for :"+sample1.name);
+        //localSearchSteepestCornersSample2.testAlgorithm(100, "Making Local Search Steepest (changing with corners) Algorithm for :"+sample2.name);
 
         localSearchSteepestCornersListMovesSample1.testAlgorithm(100, "Making Local Search Steepest (changing with corners) Algorithm using List Moves from previous step for :"+sample1.name);
         localSearchSteepestCornersListMovesSample2.testAlgorithm(100, "Making Local Search Steepest (changing with corners) Algorithm using List Moves from previous step for :"+sample2.name);
         localSearchSteepestCornersCandidatMovesSample1.testAlgorithm(100, "Making Local Search Steepest (changing with corners) Algorithm using Candidat Moves from previous step for :"+sample1.name);
         localSearchSteepestCornersCandidatMovesSample2.testAlgorithm(100, "Making Local Search Steepest (changing with corners) Algorithm using Candidat Moves from previous step for :"+sample2.name);
 
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                //new cMyFrame(sample1, greedyCycleSmaple1.listOfResults.get(greedyCycleSmaple1.minDistanceIndex-1), greedyCycleSmaple1.getClass().getName());
-                //new cMyFrame(sample2,  greedyCycleSmaple2.listOfResults.get(greedyCycleSmaple2.minDistanceIndex-1), greedyCycleSmaple2.getClass().getName());
-                //new cMyFrame(sample1,  regretHeuristicsSample1.listOfResults.get(regretHeuristicsSample1.minDistanceIndex-1), regretHeuristicsSample1.getClass().getName());
-                //new cMyFrame(sample2,  regretHeuristicsSample2.listOfResults.get(regretHeuristicsSample2.minDistanceIndex-1), regretHeuristicsSample2.getClass().getName());
-                //new cMyFrame(sample1,  localSearchGreedyPointsSample1.listOfResults.get(localSearchGreedyPointsSample1.minDistanceIndex-1), localSearchGreedyPointsSample1.getClass().getName());
-                //new cMyFrame(sample2,  localSearchGreedyPointsSample2.listOfResults.get(localSearchGreedyPointsSample2.minDistanceIndex-1), localSearchGreedyPointsSample2.getClass().getName());
-                //new cMyFrame(sample1,  localSearchGreedyCornersSample1.listOfResults.get(localSearchGreedyCornersSample1.minDistanceIndex-1), localSearchGreedyCornersSample1.getClass().getName());
-                //new cMyFrame(sample2,  localSearchGreedyCornersSample2.listOfResults.get(localSearchGreedyCornersSample2.minDistanceIndex-1), localSearchGreedyCornersSample2.getClass().getName());
-                //new cMyFrame(sample1,  localSearchSteepestPointsSample1.listOfResults.get(localSearchSteepestPointsSample1.minDistanceIndex-1), localSearchSteepestPointsSample1.getClass().getName());
-                //new cMyFrame(sample2,  localSearchSteepestPointsSample2.listOfResults.get(localSearchSteepestPointsSample2.minDistanceIndex-1), localSearchSteepestPointsSample2.getClass().getName());new cMyFrame(sample1,  localSearchSteepestCornersSample1.listOfResults.get(localSearchSteepestCornersSample1.minDistanceIndex-1), localSearchSteepestCornersSample1.getClass().getName());
-                new cMyFrame(sample1,  localSearchSteepestCornersSample1.listOfResults.get(localSearchSteepestCornersSample1.minDistanceIndex-1), localSearchSteepestCornersSample1.getClass().getName());
-                new cMyFrame(sample2,  localSearchSteepestCornersSample2.listOfResults.get(localSearchSteepestCornersSample2.minDistanceIndex-1), localSearchSteepestCornersSample2.getClass().getName());
+        EventQueue.invokeLater(() -> {
+            //new cMyFrame(sample1, greedyCycleSmaple1.listOfResults.get(greedyCycleSmaple1.minDistanceIndex-1), greedyCycleSmaple1.getClass().getName());
+            //new cMyFrame(sample2,  greedyCycleSmaple2.listOfResults.get(greedyCycleSmaple2.minDistanceIndex-1), greedyCycleSmaple2.getClass().getName());
+            //new cMyFrame(sample1,  regretHeuristicsSample1.listOfResults.get(regretHeuristicsSample1.minDistanceIndex-1), regretHeuristicsSample1.getClass().getName());
+            //new cMyFrame(sample2,  regretHeuristicsSample2.listOfResults.get(regretHeuristicsSample2.minDistanceIndex-1), regretHeuristicsSample2.getClass().getName());
+            //new cMyFrame(sample1,  localSearchGreedyPointsSample1.listOfResults.get(localSearchGreedyPointsSample1.minDistanceIndex-1), localSearchGreedyPointsSample1.getClass().getName());
+            //new cMyFrame(sample2,  localSearchGreedyPointsSample2.listOfResults.get(localSearchGreedyPointsSample2.minDistanceIndex-1), localSearchGreedyPointsSample2.getClass().getName());
+            //new cMyFrame(sample1,  localSearchGreedyCornersSample1.listOfResults.get(localSearchGreedyCornersSample1.minDistanceIndex-1), localSearchGreedyCornersSample1.getClass().getName());
+            //new cMyFrame(sample2,  localSearchGreedyCornersSample2.listOfResults.get(localSearchGreedyCornersSample2.minDistanceIndex-1), localSearchGreedyCornersSample2.getClass().getName());
+            //new cMyFrame(sample1,  localSearchSteepestPointsSample1.listOfResults.get(localSearchSteepestPointsSample1.minDistanceIndex-1), localSearchSteepestPointsSample1.getClass().getName());
+            //new cMyFrame(sample2,  localSearchSteepestPointsSample2.listOfResults.get(localSearchSteepestPointsSample2.minDistanceIndex-1), localSearchSteepestPointsSample2.getClass().getName());new cMyFrame(sample1,  localSearchSteepestCornersSample1.listOfResults.get(localSearchSteepestCornersSample1.minDistanceIndex-1), localSearchSteepestCornersSample1.getClass().getName());
+            //new cMyFrame(sample1,  localSearchSteepestCornersSample1.listOfResults.get(localSearchSteepestCornersSample1.minDistanceIndex-1), localSearchSteepestCornersSample1.getClass().getName());
+            //new cMyFrame(sample2,  localSearchSteepestCornersSample2.listOfResults.get(localSearchSteepestCornersSample2.minDistanceIndex-1), localSearchSteepestCornersSample2.getClass().getName());
 
-                new cMyFrame(sample1,  localSearchSteepestCornersListMovesSample1.listOfResults.get(localSearchSteepestCornersListMovesSample1.minDistanceIndex), localSearchSteepestCornersListMovesSample1.getClass().getName());
-                new cMyFrame(sample2,  localSearchSteepestCornersListMovesSample2.listOfResults.get(localSearchSteepestCornersListMovesSample2.minDistanceIndex), localSearchSteepestCornersListMovesSample2.getClass().getName());
-                new cMyFrame(sample1,  localSearchSteepestCornersCandidatMovesSample1.listOfResults.get(localSearchSteepestCornersCandidatMovesSample1.minDistanceIndex), localSearchSteepestCornersCandidatMovesSample1.getClass().getName());
-                new cMyFrame(sample2,  localSearchSteepestCornersCandidatMovesSample2.listOfResults.get(localSearchSteepestCornersCandidatMovesSample2.minDistanceIndex), localSearchSteepestCornersCandidatMovesSample2.getClass().getName());
-            }
+            new cMyFrame(sample1,  localSearchSteepestCornersListMovesSample1.listOfResults.get(localSearchSteepestCornersListMovesSample1.minDistanceIndex), localSearchSteepestCornersListMovesSample1.getClass().getName());
+            new cMyFrame(sample2,  localSearchSteepestCornersListMovesSample2.listOfResults.get(localSearchSteepestCornersListMovesSample2.minDistanceIndex), localSearchSteepestCornersListMovesSample2.getClass().getName());
+            new cMyFrame(sample1,  localSearchSteepestCornersCandidatMovesSample1.listOfResults.get(localSearchSteepestCornersCandidatMovesSample1.minDistanceIndex), localSearchSteepestCornersCandidatMovesSample1.getClass().getName());
+            new cMyFrame(sample2,  localSearchSteepestCornersCandidatMovesSample2.listOfResults.get(localSearchSteepestCornersCandidatMovesSample2.minDistanceIndex), localSearchSteepestCornersCandidatMovesSample2.getClass().getName());
         });
     }
 }
